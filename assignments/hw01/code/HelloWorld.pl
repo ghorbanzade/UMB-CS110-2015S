48740edb0442e4e82f837e75442b4e93ba3e5c0d
Pejman Ghorbanzade <pejman.ghorbanzade@digi.com>
2015-08-20 10:35:08 -0400

    removed hello world program in other languages

--- assignments/hw01/code/HelloWorld.pl
+++
@@ -1,9 +0,0 @@
-#
-#	Copyright (c) 2015
-#	Pejman Ghorbanzade <mail@ghorbanzade.com>
-#	CS110: Introduction to Computing
-#	Department of Computer Science
-#	University of Massachusetts Boston
-#
-
-print "Hello World!";
