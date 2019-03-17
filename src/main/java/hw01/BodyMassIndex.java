59a9129a8e2572c41294bfed90c1003668de0155
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2019-03-17 11:35:03 -0500

    updated license header block in all source files

--- src/main/java/hw01/BodyMassIndex.java
+++ src/main/java/hw01/BodyMassIndex.java
@@ -1,11 +1,8 @@
 /**
-* CS110: Introduction to Computing
-* Copyleft 2015 Pejman Ghorbanzade <mail@ghorbanzade.com>
-* More info: https://github.com/ghorbanzade/UMB-CS110-2015S
-*
-* The author has placed this file in the public domain.
-* He makes no warranty and accepts no liability for this file.
-*/
+ * UMB-CS110-2015S: Introduction to Computing
+ * Copyright 2015 Pejman Ghorbanzade <pejman@ghorbanzade.com>
+ * More info: https://github.com/ghorbanzade/UMB-CS110-2015S
+ */
 
 package edu.umb.cs110.hw03.q04;
 
